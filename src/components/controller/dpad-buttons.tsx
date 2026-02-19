db9ec40ec30591a30a1dff2fe75c5b741ab50c55
Bongane Lebopo <blebopo@dvtsoftware.com>
2026-02-19 14:06:43 +0200

    Dpand button component

---
+++ src/components/controller/dpad-buttons.tsx
@@ -0,0 +1,14 @@
+export default function DPadButtons() {
+  return (
+    <>
+      <div className="gamepad">
+        <div className="dpad">
+          <button className="up">↑</button>
+          <button className="left">←</button>
+          <button className="right">→</button>
+          <button className="down">↓</button>
+        </div>
+      </div>
+    </>
+  );
+}
