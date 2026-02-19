1f2fbc8baa3b4ca291f657a50b168da1b6954d50
Bongane Lebopo <blebopo@dvtsoftware.com>
2026-02-19 22:37:05 +0200

    Feature: Feedback screen initialization.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
 import ControllerLayoutComponent from "./components/controller/controller-layout";
 import "./App.css";
+import FeedbackLayoutComponent from "./components/feedback/feedback-layout";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div className="haptic-feedback-container"></div>
+      <div className="haptic-feedback-container">
+        <FeedbackLayoutComponent></FeedbackLayoutComponent>
+      </div>
       <div className="controller-container">
         <ControllerLayoutComponent></ControllerLayoutComponent>
       </div>
