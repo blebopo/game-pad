2880d8884fd718c59e7021918ea607ffab41470e
Bongane Lebopo <blebopo@dvtsoftware.com>
2026-02-19 22:30:15 +0200

    Feature: Keyboard input wired into action buttons.

--- src/components/controller/action-buttons.tsx
+++ src/components/controller/action-buttons.tsx
@@ -1,10 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "../../styles/action-buttons.css";
 
 type Action = "A" | "B" | "X" | "Y" | null;
 
 export default function ActionButtons() {
   const [lastPress, setLastPress] = useState<Action>(null);
+
+  useEffect(() => {
+      const actionKeyHandle = (event: KeyboardEvent) => {
+        let action: Action = null;
+        switch (event.key) {
+          case "i":
+            action = "Y";
+            break;
+          case "j":
+            action = "X";
+            break;
+          case "l":
+            action = "A";
+            break;
+          case "k":
+            action = "B";
+            break;
+        }
+        if(action){
+          handlePress(action);
+        }
+      };
+      window.addEventListener("keydown", actionKeyHandle);
+      return () => window.removeEventListener("keydown", actionKeyHandle);
+    }, []);
 
   function handlePress(action: Action) {
     setLastPress(action);
